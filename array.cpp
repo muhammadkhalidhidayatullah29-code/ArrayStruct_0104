5a15a13c32ce32e5125af71f2f0a6faad24ed7f9
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-07 17:21:18 +0700

    cara akses membaca, mengambil, mengisi array

--- array.cpp
+++ array.cpp
@@ -5,4 +5,8 @@ int main()
 {
     int dataku[5] = {10, 20, 30, 40, 50};
     int datamu[5];
+
+    cout << "Isi data kedua : " << dataku[1] << endl;
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
 }
