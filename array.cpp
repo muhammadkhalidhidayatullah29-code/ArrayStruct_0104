c58e883e24183c8a703c55386d78e5577181d00f
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-07 17:27:12 +0700

    mengisi data dengan perintah satu persatu

--- array.cpp
+++ array.cpp
@@ -9,4 +9,15 @@ int main()
     cout << "Isi data kedua : " << dataku[1] << endl;
     dataku[1] = 200;
     cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
+
+    cout << "isikan data pertama: ";
+    cin >> datamu[0];
+    cout << "isikan data kedua: ";
+    cin >> datamu[1];
+    cout << "isikan data ketiga: ";
+    cin >> datamu[2];
+    cout << "isikan data keempat: ";
+    cin >> datamu[3];
+    cout << "isikan data kelima: ";
+    cin >> datamu[4];
 }
