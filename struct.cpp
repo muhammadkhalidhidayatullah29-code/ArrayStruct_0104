7178f41dcee61b93b2179271cd2928addf5e1cbd
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-07 18:20:41 +0700

    deklarasi variabel struct

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,21 @@ struct Orang
     string alamat;
     int umur;
 };
+
+int main()
+{
+    Orang mhs;
+
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout <<"Data Tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Usia : " << mhs.umur << endl;
+}
